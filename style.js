3eac1e78698add4a72f7958867bc6db078b0a5c5
nataliagomes <naty.pinheiro95@gmail.com>
2019-07-15 17:14:08 -0300

    criarjs3

---
+++ style.js
@@ -0,0 +1,28 @@
+/*let numeros= {5,7,9,0} for (let i=99; i >=0; i--) { console.log(i);} usando o FOROF FORIN
+   
+ let filmes= [
+     {titulo: 'Harry Potter', classificacao :12},
+     {titulo: 'As Branquelas', classificacao: 16},
+     {titulo:'Rei Leão',classificacao: 10},
+     {titulo: 'interestelar', classificacao: 10},
+     {titulo:  'Ninja Assasino', classificacao: 18},
+     {titulo: 'John Wick', classificacao: 16},
+     {titulo:'velozes e Furiosos', classificacao: 14} ];
+
+     let idade= Number(prompt ('digite a idade'));
+
+     for(let indice in filmes){
+         if(idade>=filmes [indice].classificacao){
+             console.log(filmes[indice].titulo);
+         }
+    } document.querySelector("body").onmouseleave=function(){ alert( "nãooooooooooo saiaaaaaaaaaaa");}
+*/
+   let fora= document.queryCommandValue("fora");
+   let dentro= document.querySelector("dentro");
+   
+fora.onmousemove = function (){
+    for(let quadrado of dentro){
+        quadrado.style.backgroundColor= `rgb(${aleatorio (o,255)},$
+        {aleatorio (0,255)},${aleatorio (o,225)`};
+    }
+}
